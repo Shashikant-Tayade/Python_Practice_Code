9f177f9a1034b12d715496cf974dfb0c9a62957f
shashikant tayade <shashibtayade@gmail.com>
2026-03-14 14:51:13 +0530

    Swapping Using Unpacking

---
+++ Swapping_using Unpacking.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the num1 : "))
+num2 = int(input("Enter the num2 : "))
+
+print(f"Before swap : num1 = {num1}, num2 = {num2}")
+
+num1, num2 = num2, num1
+
+print(f"BefAfterore swap : num1 = {num1}, num2 = {num2}")
